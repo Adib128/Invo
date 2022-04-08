efbba0dc4308f9d4a09fffb59058786a65d8a9cc
Adib128 <aouadi.adib@gmail.com>
2022-04-09 00:27:03 +0100

    User controller and route added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends BaseController
+{
+    public function register(UserRequest $request)
+    {
+        $request['password'] = bcrypt($request->password);
+
+        $user = User::create($request->all());
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return $this->handleResponse(
+            ['user' => $user, 'access_token' => $accessToken],
+            'User logged successfully'
+        );
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+        if (!auth()->attempt($loginData)) {
+            return $this->handleError('Invalid Credentials');
+        }
+        $accessToken = auth()
+            ->user()
+            ->createToken('authToken')->accessToken;
+
+        return $this->handleResponse(
+            ['user' => auth()->user(), 'access_token' => $accessToken],
+            'User logged successfully'
+        );
+    }
+
+    public function profile()
+    {
+        return $this->handleResponse(auth()->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:api');
 
-Route::apiResource('/customers', CustomerController::class);
+Route::apiResource('/customers', CustomerController::class)->middleware('auth:api');

c7f81f067e3347342101f56afe8095cf015523f4
Adib128 <aouadi.adib@gmail.com>
2022-04-23 13:21:23 +0100

    Change password and logout test methods added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,22 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *    title="Invoice Management REST API",
- *    version="1.0.0",
- * ),
- * @OA\Server(url=L5_SWAGGER_CONST_HOST)
- *   @OA\SecurityScheme(
- *       securityScheme="bearerAuth",
- *       in="header",
- *       name="bearerAuth",
- *       type="http",
- *       scheme="bearer",
- *       bearerFormat="JWT",
- *    ),
- */
-
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -10,7 +10,6 @@ class AuthTest extends TestCase
 {
     use WithFaker;
 
-
     public function testRegister()
     {
         $user = [
@@ -46,7 +45,7 @@ class AuthTest extends TestCase
         $user = User::factory()->create();
         $userData = [
             'email' => $user->email,
-            'password' => 'password'
+            'password' => 'password',
         ];
         $response = $this->json('POST', '/api/login', $userData);
         $response->assertStatus(200);
@@ -63,6 +62,92 @@ class AuthTest extends TestCase
             'password' => $this->faker->password,
         ];
         $response = $this->json('POST', '/api/login', $userData);
+        $response->assertStatus(401);
+    }
+
+    public function testChangePassword()
+    {
+        $user = User::factory()->create();
+        auth()->attempt([
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+        $token = auth()
+            ->user()
+            ->createToken('authToken')->accessToken;
+
+        $payload = [
+            'current_password' => 'password',
+            'new_password' => '12345678',
+            'new_confirm_password' => '12345678',
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', '/api/change-password', $payload);
+
+        $response->assertStatus(200);
+
+        $response->assertExactJson([
+            'success' => true,
+            'message' => 'Password change successfully',
+        ]);
+    }
+
+    public function testInvalidChangePassword()
+    {
+        $token = $this->authenticate();
+        $payload = [
+            'current_password' => 'password123',
+            'new_password' => 'a115f5f5s',
+            'new_confirm_password' => '12a598g88g',
+        ];
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', '/api/change-password', $payload);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'success',
+            'errors' => ['current_password', 'new_confirm_password'],
+        ]);
+    }
+
+    public function testLogout()
+    {
+        $token = $this->authenticate();
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', '/api/logout');
+
+        $response->assertStatus(200);
+
+        $response->assertExactJson([
+            'success' => true,
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function testInvalidLogout()
+    {
+        $token  = $this->faker->text();
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', '/api/logout');
+
+        $response->assertStatus(401);
+    }
+
+    public function testInvalidAccess()
+    {
+        $response = $this->json('GET', '/api/products');
+        $response->assertStatus(401);
+
+        $response = $this->json('GET', '/api/invoices');
+        $response->assertStatus(401);
+
+        $response = $this->json('GET', '/api/customers');
         $response->assertStatus(401);
     }
 }
